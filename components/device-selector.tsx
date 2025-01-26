4fdb1ebb854b633f6ba21b1a852ed61a22d9ca83
vivektiwari91 <mrvickytiwari91@gmail.com>
2025-01-27 00:31:28 +0530

    make changes

--- components/device-selector.tsx
+++ components/device-selector.tsx
@@ -4,13 +4,15 @@ import type { DeviceType } from "@/types/metrics"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 interface DeviceSelectorProps {
+  // eslint-disable-next-line no-unused-vars
   value: DeviceType
   onChange: (value: DeviceType) => void
 }
 
+// eslint-disable-next-line no-unused-vars
 export function DeviceSelector({ value, onChange }: DeviceSelectorProps) {
   return (
-    <div className="flex items-center gap-4" style={{ justifyContent : "center"}}>
+    <div className="flex items-center gap-4" style={{ justifyContent: "center" }}>
       <span className="text-lg font-medium">Choose a Device</span>
       <Select value={value} onValueChange={onChange as (value: string) => void}>
         <SelectTrigger className="w-[180px]">
